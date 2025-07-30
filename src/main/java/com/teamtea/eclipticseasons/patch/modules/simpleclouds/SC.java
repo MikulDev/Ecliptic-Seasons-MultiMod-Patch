108555a03411b08fde52ea5d228ea5f3357b7065
joe-vettek <my1233215@126.com>
2025-07-30 15:07:09 +0800

    0.8

---
+++ src/main/java/com/teamtea/eclipticseasons/patch/modules/simpleclouds/SC.java
@@ -0,0 +1,26 @@
+package com.teamtea.eclipticseasons.patch.modules.simpleclouds;
+
+import com.teamtea.eclipticseasons.patch.api.ESPatch;
+import com.teamtea.eclipticseasons.patch.api.IESModPatch;
+import net.minecraftforge.common.ForgeConfigSpec;
+
+
+@ESPatch(mods = "simpleclouds")
+public class SC implements IESModPatch {
+
+    @Override
+    public void common(ForgeConfigSpec.Builder consumer) {
+        Config.load(consumer);
+    }
+
+    public static class Config {
+
+        public static ForgeConfigSpec.BooleanValue enable;
+
+        public static void load(ForgeConfigSpec.Builder builder) {
+            builder.comment("Simple Clouds","This is just a simple processing routine and is not fully compatible.").push("simpleclouds");
+            enable = builder.define("Enable", true);
+            builder.pop();
+        }
+    }
+}
