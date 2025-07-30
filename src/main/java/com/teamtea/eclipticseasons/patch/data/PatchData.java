b77dc5027342dc93b8db2a2df5e87ca9d7fc723d
joe-vettek <my1233215@126.com>
2025-07-30 12:44:22 +0800

    0.6

--- src/main/java/com/teamtea/eclipticseasons/patch/data/PatchData.java
+++ src/main/java/com/teamtea/eclipticseasons/patch/data/PatchData.java
@@ -1,12 +1,28 @@
 package com.teamtea.eclipticseasons.patch.data;
 
-import com.teamtea.eclipticseasons.EclipticSeasons;
+import com.teamtea.eclipticseasons.data.api.MutablePackOutput;
+import com.teamtea.eclipticseasons.patch.EclipticSeasonsPatch;
+import com.teamtea.eclipticseasons.patch.data.lang.Lang_EN;
+import com.teamtea.eclipticseasons.patch.data.lang.Lang_ZH;
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.data.event.GatherDataEvent;
 
 
 public class PatchData {
-    public final static String MODID = EclipticSeasons.MODID;
+    public final static String MODID = EclipticSeasonsPatch.MODID;
 
     public static void dataGen(GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+        ExistingFileHelper helper = event.getExistingFileHelper();
+        MutablePackOutput packOutput = new MutablePackOutput(generator.getPackOutput());
+        if (event.includeServer()) {
+
+        }
+        if (event.includeClient()) {
+            generator.addProvider(event.includeClient(), new Lang_EN(packOutput, helper));
+            generator.addProvider(event.includeClient(), new Lang_ZH(packOutput, helper));
+
+        }
     }
 }
---
+++ src/main/java/com/teamtea/eclipticseasons/patch/data/lang/Lang_EN.java
@@ -0,0 +1,31 @@
+package com.teamtea.eclipticseasons.patch.data.lang;
+
+import com.teamtea.eclipticseasons.data.general.lang.LangHelper;
+import com.teamtea.eclipticseasons.patch.EclipticSeasonsPatch;
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.common.data.ExistingFileHelper;
+
+
+public class Lang_EN extends LangHelper {
+    public Lang_EN(PackOutput gen, ExistingFileHelper helper) {
+        super(gen, helper, EclipticSeasonsPatch.MODID, "en_us");
+    }
+
+
+    @Override
+    protected void addTranslations() {
+        addTouhouLittleMaid();
+        addConfigLang();
+    }
+
+
+    private void addTouhouLittleMaid() {
+        add("task.eclipticseasons.clean_snow", "Clean Snow");
+        add("task.eclipticseasons.clean_snow.desc", "Applied to snow-covered blocks from Ecliptic Season");
+        add("task.eclipticseasons.clean_snow.condition.has_broom", "Has Broom");
+    }
+
+    private void addConfigLang() {
+
+    }
+}
